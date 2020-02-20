2c7d043b79c4d00a084de8e506d4b27219b483f1
Remi <rku16@ic.ac.uk>
2020-02-20 18:38:41 +0000

    Main can load any data file from command line

--- FirstRound/main.py
+++ FirstRound/main.py
@@ -1,4 +1,21 @@
-from dataReader import reader
+import argparse
+
+from dataHandler import reader, parser
 
 if __name__ == "__main__":
-    reader("data/a_example.txt")
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--f', help="letter_to_load", type=str, default='a')
+    args = argparser.parse_args()
+
+    filename = {
+        'a': 'data/a_example.txt',
+        'b': 'data/b_read_on.txt',
+        'c': 'data/c_incunabula.txt',
+        'd': 'data/d_tough_choices.txt',
+        'e': 'data/e_so_many_books.txt',
+        'f': 'data/f_libraries_of_the_world.txt'
+    }[args.f]
+
+    b, l = parser(reader(filename))
+    print(f"Books \n{b}\nLibraries\n{l}")
